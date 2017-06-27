3980941cb540481371d106d828f100c6053c96b1
atlas <atlas@localhost.localdomain>
2017-06-27 23:49:00 +0530

    first commit

---
+++ akfuzz.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import socket
+import sys
+
+print " prova "
+
+
+buffer=["A"]
+counter=20
+while len(buffer) <= 30:
+	buffer.append("A"*counter)
+	counter=counter+100
+
+commands=["MKD","CWD","STOR"]
+print "Accepting your given commands"
+for command in commands:
+	for string in buffer:
+		print "Fuzzing " + command + " with length:" +str(len(string))
+		s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		connect=s.connect(('0.0.0.0',21))
+		s.recv(1024)
+		s.send('USER ftp\r\n')
+		s.recv(1024)
+		s.send('PASS ftp\r\n')
+		s.recv(1024)
+		s.send(command + ' ' + string + '\r\n')
+		s.recv(1024)
+		s.send('QUIT\r\n')
+		s.close()
